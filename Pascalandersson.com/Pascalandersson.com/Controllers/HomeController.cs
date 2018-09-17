5b826f377ebf02bb98802dc1d4bd06042cd85595
Pascal Andersson <anderssonpascal@gmail.com>
2018-09-17 14:59:47 +0200

    Initial commit
    Added Umbraco CMS to solution.

---
+++ Pascalandersson.com/Pascalandersson.com/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Pascalandersson.com.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace Pascalandersson.com.Controllers
+{
+    public class HomeController: SurfaceController, IRenderMvcController
+    {
+        public ActionResult Index(RenderModel renderModel)
+        {
+            var model = new HomeViewModel();
+            return View(model);
+        }
+    }
+}
---
+++ Pascalandersson.com/Pascalandersson.com/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+
+namespace Pascalandersson.com.Models.ViewModels
+{
+    public class HomeViewModel : RenderModel
+    {
+        public HomeViewModel() : this(UmbracoContext.Current.PublishedContentRequest.PublishedContent) { }
+
+        public HomeViewModel(IPublishedContent content) : base(content)
+        {
+
+        }
+    }
+}
