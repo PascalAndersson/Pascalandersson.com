0fa25d17cfde312706b1fbcec9b8c529d0c6d13e
Pascal Andersson <anderssonpascal@gmail.com>
2018-09-24 12:15:30 +0200

    Navbar
    Added navbar scrolling effect with js.

--- Pascalandersson.com/Pascalandersson.com/Static/Scripts/navbar.js
+++ Pascalandersson.com/Pascalandersson.com/Static/Scripts/navbar.js
@@ -7,7 +7,7 @@ function displayScrolledNavbar() {
     var heroImageHeight = $("#heroImage").height();
     var navbarContainer = $(".navbar__container");
 
-    if ($(window).scrollTop() > (heroImageHeight - 80)) {
+    if ($(window).scrollTop() > (heroImageHeight - 700)) {
         $(navbarContainer).addClass("scrolled-down");
     } else {
         $(navbarContainer).removeClass("scrolled-down");
